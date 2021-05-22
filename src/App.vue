9f1d8157573795c449033ab4e6312ee14c9955d2
oliveiradiney <dineyoliveira1999@gmail.com>
2021-05-22 02:23:39 -0300

    Adicionando e listando Todos

--- src/App.vue
+++ src/App.vue
@@ -9,8 +9,23 @@
                   <input type="text" v-model="todo.description" class="form-input" placeholder="Novo todo">
                   <button class="btn btn-primary input-group-btn">Adicionar</button>
               </div>
-              {{ todos }}
           </form>
+          <div class="todo-list">
+              <div class="tile" v-for="t in todos" :key="t.id">
+                      <div class="tile-icon">
+                            <i class="icon icon-time flex-centered"></i>
+                      </div>
+                      <div class="tile-content">
+                            <div class="tile-subtitle">{{ t.description }}</div>
+                      </div>
+                      <div class="tile-action">
+                          <button class="btn btn-link">Concluído</button>
+                          <button class="btn btn-link">
+                              <span class="text-error">Remover</span>
+                          </button>
+                      </div>
+              </div>
+          </div>
       </div>
   </div>
 </template>
@@ -27,6 +42,7 @@ export default {
     addTodo(todo) {
       todo.id = Date.now();
       this.todos.push(todo)
+      this.todo = {checked: false}
     }
   }
 };
@@ -35,6 +51,9 @@ export default {
   .img-logo{
     max-width: 200px;
     margin: 0 auto;
+  }
+  .todo-list{
+    padding-top: 2rem;
   }
 </style>
 
